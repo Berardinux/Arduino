d02727c9681a72db92e6db9510b0fddf2404dc68
Berardinux <berardinux@gmail.com>
2023-12-14 20:38:56 -0500

    Dec 14, 20:38

---
+++ Project02/Project02Challenge.ino
@@ -0,0 +1,26 @@
+int varSwitchState = 0;
+void setup() {
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
+
+  pinMode(2, INPUT);
+}
+
+void loop() {
+  varSwitchState = digitalRead(2);
+
+  if (varSwitchState == LOW) {
+    digitalWrite(3, HIGH);
+    digitalWrite(4, LOW);
+    digitalWrite(5, LOW);
+  } else {
+    digitalWrite(3, LOW);
+    digitalWrite(4, LOW);
+    digitalWrite(5, HIGH);
+    delay(250);
+    digitalWrite(4, HIGH);
+    digitalWrite(5, LOW);
+    delay(250);
+  }
+}
